c60de511bf2d4e59a215a0cd203c8622fa15a750
rendel <renaud@sindicetech.com>
2014-10-14 12:12:58 +0100

    Added checkpoint actions

--- solr/core/src/test/org/apache/solr/cloud/CdcrRequestHandlerTest.java
+++ solr/core/src/test/org/apache/solr/cloud/CdcrRequestHandlerTest.java
@@ -34,6 +34,7 @@ public class CdcrRequestHandlerTest extends BasicDistributedZkTest {
   @Override
   @Before
   public void setUp() throws Exception {
+    schemaString = "schema15.xml";      // we need a string id
     fixShardCount = true;
     sliceCount = 2;
     shardCount = 2;
@@ -42,7 +43,8 @@ public class CdcrRequestHandlerTest extends BasicDistributedZkTest {
 
   @Override
   public void doTest() throws Exception {
-    this.doTestLifeCycleActions();
+//    this.doTestLifeCycleActions();
+    this.doTestCheckpointActions();
   }
 
   // check that the life-cycle state is properly synchronised across nodes
@@ -83,6 +85,53 @@ public class CdcrRequestHandlerTest extends BasicDistributedZkTest {
     rsp = sendRequest(SHARD2, CdcrRequestHandler.CdcrAction.STATUS);
     status = (String) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
     assertEquals(state.toLower(), status);
+  }
+
+  // check
+  public void doTestCheckpointActions() throws Exception {
+    try {
+      // initial request on an empty index, must return -1
+      NamedList rsp = sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.COLLECTIONCHECKPOINT);
+      assertEquals(-1l, rsp.get("checkpoint"));
+
+      index(id, "a"); // shard 2
+      commit();
+
+      // only one document indexed in shard 2, the checkpoint must be still -1
+      rsp = sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.COLLECTIONCHECKPOINT);
+      assertEquals(-1l, rsp.get("checkpoint"));
+
+      index(id, "b"); // shard 1
+      commit();
+
+      // a second document indexed in shard 1, the checkpoint must come from shard 2
+      rsp = sendRequest(SHARD2, CdcrRequestHandler.CdcrAction.COLLECTIONCHECKPOINT);
+      long checkpoint1 = (Long) rsp.get("checkpoint");
+      long expected = (Long) sendRequest(SHARD2, CdcrRequestHandler.CdcrAction.SLICECHECKPOINT).get("checkpoint");
+      assertEquals(expected, checkpoint1);
+
+      index(id, "c"); // shard 1
+      commit();
+
+      // a third document indexed in shard 1, the checkpoint must still come from shard 2
+      rsp = sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.COLLECTIONCHECKPOINT);
+      assertEquals(checkpoint1, rsp.get("checkpoint"));
+
+      index(id, "d"); // shard 2
+      commit();
+
+      // a fourth document indexed in shard 2, the checkpoint must come from shard 1
+      rsp = sendRequest(SHARD2, CdcrRequestHandler.CdcrAction.COLLECTIONCHECKPOINT);
+      long checkpoint2 = (Long) rsp.get("checkpoint");
+      expected = (Long) sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.SLICECHECKPOINT).get("checkpoint");
+      assertEquals(expected, checkpoint2);
+    }
+    catch (SolrServerException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   protected NamedList sendRequest(String shardId, CdcrRequestHandler.CdcrAction action)
