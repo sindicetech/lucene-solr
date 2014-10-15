364a1956c633c9b97840033e65d869d6184567a2
rendel <renaud@sindicetech.com>
2014-10-15 19:11:53 +0100

    Added buffer state synchronisation

--- solr/core/src/test/org/apache/solr/cloud/CdcrRequestHandlerTest.java
+++ solr/core/src/test/org/apache/solr/cloud/CdcrRequestHandlerTest.java
@@ -45,29 +45,30 @@ public class CdcrRequestHandlerTest extends BasicDistributedZkTest {
   public void doTest() throws Exception {
     this.doTestLifeCycleActions();
     this.doTestCheckpointActions();
+    this.doTestBufferActions();
   }
 
   // check that the life-cycle state is properly synchronised across nodes
   public void doTestLifeCycleActions(){
     try {
       // check initial status
-      this.assertState(CdcrRequestHandler.CdcrState.STOPPED);
+      this.assertState(CdcrRequestHandler.ProcessState.STOPPED, CdcrRequestHandler.BufferState.ENABLED);
 
-      // send start action
+      // send start action to first shard
       NamedList rsp = sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.START);
-      String status = (String) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
-      assertEquals(CdcrRequestHandler.CdcrState.STARTED.toLower(), status);
+      NamedList status = (NamedList) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
+      assertEquals(CdcrRequestHandler.ProcessState.STARTED.toLower(), status.get(CdcrRequestHandler.ProcessState.getParam()));
 
       // check status
-      this.assertState(CdcrRequestHandler.CdcrState.STARTED);
+      this.assertState(CdcrRequestHandler.ProcessState.STARTED, CdcrRequestHandler.BufferState.ENABLED);
 
-      // send stop action
-      rsp = sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.STOP);
-      status = (String) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
-      assertEquals(CdcrRequestHandler.CdcrState.STOPPED.toLower(), status);
+      // send stop action to second shard
+      rsp = sendRequest(SHARD2, CdcrRequestHandler.CdcrAction.STOP);
+      status = (NamedList) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
+      assertEquals(CdcrRequestHandler.ProcessState.STOPPED.toLower(), status.get(CdcrRequestHandler.ProcessState.getParam()));
 
       // check status
-      this.assertState(CdcrRequestHandler.CdcrState.STOPPED);
+      this.assertState(CdcrRequestHandler.ProcessState.STOPPED, CdcrRequestHandler.BufferState.ENABLED);
     }
     catch (SolrServerException e) {
       e.printStackTrace();
@@ -77,17 +78,20 @@ public class CdcrRequestHandlerTest extends BasicDistributedZkTest {
     }
   }
 
-  private void assertState(CdcrRequestHandler.CdcrState state) throws IOException, SolrServerException {
+  private void assertState(CdcrRequestHandler.ProcessState processState, CdcrRequestHandler.BufferState bufferState)
+  throws IOException, SolrServerException {
     NamedList rsp = sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.STATUS);
-    String status = (String) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
-    assertEquals(state.toLower(), status);
+    NamedList status = (NamedList) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
+    assertEquals(processState.toLower(), status.get(CdcrRequestHandler.ProcessState.getParam()));
+    assertEquals(bufferState.toLower(), status.get(CdcrRequestHandler.BufferState.getParam()));
 
     rsp = sendRequest(SHARD2, CdcrRequestHandler.CdcrAction.STATUS);
-    status = (String) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
-    assertEquals(state.toLower(), status);
+    status = (NamedList) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
+    assertEquals(processState.toLower(), status.get(CdcrRequestHandler.ProcessState.getParam()));
+    assertEquals(bufferState.toLower(), status.get(CdcrRequestHandler.BufferState.getParam()));
   }
 
-  // check
+  // check the checkpoint API
   public void doTestCheckpointActions() throws Exception {
     try {
       // initial request on an empty index, must return -1
@@ -125,6 +129,36 @@ public class CdcrRequestHandlerTest extends BasicDistributedZkTest {
       long checkpoint2 = (Long) rsp.get("checkpoint");
       expected = (Long) sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.SLICECHECKPOINT).get("checkpoint");
       assertEquals(expected, checkpoint2);
+    }
+    catch (SolrServerException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  // check that the buffer state is properly synchronised across nodes
+  public void doTestBufferActions(){
+    try {
+      // check initial status
+      this.assertState(CdcrRequestHandler.ProcessState.STOPPED, CdcrRequestHandler.BufferState.ENABLED);
+
+      // send disable buffer action to first shard
+      NamedList rsp = sendRequest(SHARD1, CdcrRequestHandler.CdcrAction.DISABLEBUFFER);
+      NamedList status = (NamedList) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
+      assertEquals(CdcrRequestHandler.BufferState.DISABLED.toLower(), status.get(CdcrRequestHandler.BufferState.getParam()));
+
+      // check status
+      this.assertState(CdcrRequestHandler.ProcessState.STOPPED, CdcrRequestHandler.BufferState.DISABLED);
+
+      // send enable buffer action to second shard
+      rsp = sendRequest(SHARD2, CdcrRequestHandler.CdcrAction.ENABLEBUFFER);
+      status = (NamedList) rsp.get(CdcrRequestHandler.CdcrAction.STATUS.toLower());
+      assertEquals(CdcrRequestHandler.BufferState.ENABLED.toLower(), status.get(CdcrRequestHandler.BufferState.getParam()));
+
+      // check status
+      this.assertState(CdcrRequestHandler.ProcessState.STOPPED, CdcrRequestHandler.BufferState.ENABLED);
     }
     catch (SolrServerException e) {
       e.printStackTrace();
