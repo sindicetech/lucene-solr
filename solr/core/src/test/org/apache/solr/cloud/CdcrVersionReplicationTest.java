ac959399e25aecc593d28c2a7e8394754378e967
Jakub Kotowski <jakub@kotowski.cz>
2014-10-23 22:02:17 +0100

    CDCR deleteByQuery version replication

--- solr/core/src/test/org/apache/solr/cloud/CdcrVersionReplicationTest.java
+++ solr/core/src/test/org/apache/solr/cloud/CdcrVersionReplicationTest.java
@@ -25,18 +25,22 @@ import java.util.Set;
 
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CloudSolrServer;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.request.UpdateRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrException;
+import org.apache.solr.common.cloud.Replica;
+import org.apache.solr.common.cloud.Slice;
+import org.apache.solr.common.cloud.ZkStateReader;
 import org.apache.solr.common.util.StrUtils;
 import org.apache.solr.update.processor.CdcrUpdateProcessor;
 import org.apache.solr.update.processor.DistributedUpdateProcessor;
 
 
-public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
-  private static String vfield = DistributedUpdateProcessor.VERSION_FIELD;
+public class CdcrVersionReplicationTest extends AbstractCdcrDistributedZkTest {
+  private static final String vfield = DistributedUpdateProcessor.VERSION_FIELD;
   SolrServer solrServer;
 
   /*
@@ -50,34 +54,42 @@ public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
     return "solrconfig-tlog-cdcr.xml";
   }
 
-  public CdcrVersionPreservationTest() {
+  public CdcrVersionReplicationTest() {
     schemaString = "schema15.xml";      // we need a string id
-    //super.sliceCount = 2;
-    //super.shardCount = 4;
-    //super.fixShardCount = true;  // we only want to test with exactly 2 slices.
+    super.sliceCount = 1;
+    super.shardCount = 2;
+    super.fixShardCount = true;  // we only want to test with exactly 2 slices.
+  }
+
+  SolrServer createClientRandomly() throws Exception {
+    int r = 1;//random().nextInt(100);
+
+    // testing the smart cloud client (requests to leaders) is more important than testing the forwarding logic
+    if (r < 80) {
+      return createCloudClient(DEFAULT_COLLECTION);
+    }
+
+    if (r < 90) {
+      return getReplicaClient(DEFAULT_COLLECTION, "shard1");
+    }
+
+    return getReplicaClient(DEFAULT_COLLECTION, "shard2");
   }
 
   @Override
   public void doTest() throws Exception {
     this.printLayout();
     boolean testFinished = false;
+    SolrServer client = null;
     try {
       handle.clear();
       handle.put("timestamp", SKIPVAL);
 
       // todo: do I have to do this here?
-      waitForRecoveriesToFinish(false);
+      waitForRecoveriesToFinish(DEFAULT_COLLECTION, false);
 
-      // doTestCdcrDocVersions(cloudClient);
-
-      //
-      // now test with a non-smart client
-      //
-      // use a leader so we test both forwarding and non-forwarding logic
-      doTestCdcrDocVersions(shardToLeaderJetty.get("shard1").client.solrClient);
-
-      // now test forwarding of the other half of commands
-      // doTestCdcrDocVersions(shardToLeaderJetty.get("shard2").client.solrClient);
+      client = createClientRandomly();
+      doTestCdcrDocVersions(client);
 
       commit(); // work arround SOLR-5628
 
@@ -86,6 +98,10 @@ public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
       if (!testFinished) {
         printLayoutOnTearDown = true;
       }
+
+      if (client != null) {
+        client.shutdown();
+      }
     }
   }
 
@@ -93,7 +109,7 @@ public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
     this.solrServer = solrServer;
 
     log.info("### STARTING doCdcrTestDocVersions - Add commands, client: " + solrServer);
-    assertEquals(2, cloudClient.getZkStateReader().getClusterState().getCollection(DEFAULT_COLLECTION).getSlices().size());
+//    assertEquals(2, cloudClient.getZkStateReader().getClusterState().getCollection(DEFAULT_COLLECTION).getSlices().size());
 
     vadd("doc1", 10, CdcrUpdateProcessor.CDCR_UPDATE, "");
     vadd("doc2", 11, CdcrUpdateProcessor.CDCR_UPDATE, "");
@@ -133,7 +149,7 @@ public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
 
     // query all shard replicas individually
     doQueryShard("shard1", "doc1,12,doc2,12,doc3,12,doc4,12", "q","*:*");
-    doQueryShard("shard2", "doc1,12,doc2,12,doc3,12,doc4,12", "q","*:*");
+//    doQueryShard("shard2", "doc1,12,doc2,12,doc3,12,doc4,12", "q","*:*");
 
     // optimistic locking update
     vadd("doc4", 12);
@@ -166,17 +182,25 @@ public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
 
     // query each shard replica individually
     doQueryShard("shard1", "doc2,12,doc3,12", "q","*:*");
-    doQueryShard("shard2", "doc2,12,doc3,12", "q","*:*");
+//    doQueryShard("shard2", "doc2,12,doc3,12", "q","*:*");
 
     // version conflict thanks to optimistic locking
-    if (solrServer.equals(cloudClient)) // TODO: it seems that optimistic locking doesn't work with forwarding, test with shard2 client
+    if (solrServer instanceof CloudSolrServer) // TODO: it seems that optimistic locking doesn't work with forwarding, test with shard2 client
       vdeleteFail("doc2", 50, 409);
 
     // cleanup after ourselves for the next run
-    vdelete("doc2", 50, CdcrUpdateProcessor.CDCR_UPDATE, "");
-    vdelete("doc3", 50, CdcrUpdateProcessor.CDCR_UPDATE, "");
-
+    // deleteByQuery should work as usual with the CDCR_UPDATE param
+    doDeleteByQuery("id:doc*", CdcrUpdateProcessor.CDCR_UPDATE, "", vfield, Long.toString(1));
     commit();
+
+    // deleteByQuery with a version lower than anything else should have no effect
+    doQuery(solrServer, "doc2,12,doc3,12", "q","*:*");
+
+    doDeleteByQuery("id:doc*", CdcrUpdateProcessor.CDCR_UPDATE, "", vfield, Long.toString(51));
+    commit();
+
+    // deleteByQuery with a version higher than everything else should delete all remaining docs
+    doQuery(solrServer, "", "q","*:*");
   }
 
 
@@ -213,6 +237,9 @@ public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
       vdelete(id, version, params);
     } catch (SolrException e) {
       failed = true;
+      if (e.getCause() instanceof SolrException && e.getCause() != e) {
+        e = (SolrException) e.getCause();
+      }
       assertEquals(errCode, e.code());
     } catch (SolrServerException ex) {
       Throwable t = ex.getCause();
@@ -242,6 +269,9 @@ public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
       vadd(id, version, params);
     } catch (SolrException e) {
       failed = true;
+      if (e.getCause() instanceof SolrException && e.getCause() != e) {
+        e = (SolrException) e.getCause();
+      }
       assertEquals(errCode, e.code());
     } catch (SolrServerException ex) {
       Throwable t = ex.getCause();
@@ -319,7 +349,7 @@ public class CdcrVersionPreservationTest extends AbstractCdcrDistributedZkTest {
   }
 
 
-  void doDBQ(String q, String... reqParams) throws Exception {
+  void doDeleteByQuery(String q, String... reqParams) throws Exception {
     UpdateRequest req = new UpdateRequest();
     req.deleteByQuery(q);
     req.setParams(params(reqParams));
